42ba2e80b85a824d6178f42a03037b71f74fa262
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-03-09 16:30:26 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).on('ready', function(){
+
+    var els = {
+        panels: $('.panel')
+    };
+
+    els.panels.on('mouseenter', function(e){
+        TweenMax.to(e.target, 0.5, { 'flex-grow': '5' })
+    });
+
+    els.panels.on('mouseleave', function(e){
+        TweenMax.to(e.target, 0.5, { 'flex-grow': '1' })
+    });
+
+});
