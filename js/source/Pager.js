f68fd01b81e88a59042b4ead6acef6dbef16da31
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-03-21 13:26:15 +0000

    multipage site

--- js/source/Pager.js
+++ js/source/Pager.js
@@ -1,5 +1,14 @@
+import $ from 'jquery';
+
+const baseTemplateFolder = './templates/';
+
 export default class Pager {
     constructor(page) {
         this.page = page;
+    }
+    
+    fillContainer(el) {
+        let file = this.page + '.html';
+        $(el).load(baseTemplateFolder + file);
     }
 }
--- js/source/main.js
+++ js/source/main.js
@@ -11,15 +11,22 @@ class App {
     initializeElements() {
         this.$els = {
             document: $(document),
-            body: $('body')
+            window: $(window),
+            body: $('body'),
+            sectionContainer: $('.sectionContainer')
         }
     }
     
     initializeListeners() {
         this.$els.document.on('ready', this.onDocumentReady.bind(this));
+        this.$els.window.on('hashchange', this.onPageChange.bind(this));
     }
     
     onDocumentReady() {
+        this.detectPage();
+    }
+    
+    detectPage() {
         this.router = new Router(window.location.href);
         this.router.getPage(this.onPageDetected.bind(this));
     }
@@ -30,8 +37,15 @@ class App {
         if(error) {
             page = 'projet';
         }
-        
-        new Pager(page);
+        if(this.page !== page) {
+            let pager = new Pager(page);
+            pager.fillContainer(this.$els.sectionContainer);
+        }
+        this.page = page;
+    }
+    
+    onPageChange() {
+        this.detectPage();
     }
 }
 
