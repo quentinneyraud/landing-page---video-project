aa0826e3a690ba760888bec79eda9e6af6d70c7f
Mickael Chanrion <mickael@noe-interactive.com>
2016-03-21 09:23:04 +0100

    JS Object

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-$(document).on('ready', function () {
+/*$(document).on('ready', function () {
 
-    /*var els = {
+    /!*var els = {
         panels: $('.panel')
     };
      els.panels.on('mouseenter', function (e) {
@@ -10,5 +10,19 @@ $(document).on('ready', function () {
     });
      els.panels.on('mouseleave', function (e) {
         TweenMax.to(e.target, 0.5, { 'flex-grow': '1' });
-    });*/
-});
+    });*!/
+});*/
+
+(function($, T) {
+    var App = {
+        el: $('body'),
+
+        init: function () {
+            console.log('lalala');
+            //T.to(this.el, 0.5, {opacity: 0});
+        }
+    }
+
+    App.init();
+
+})(jQuery, TweenMax);
